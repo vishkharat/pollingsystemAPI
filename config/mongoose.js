31d964361ec3e57f652bd3e044590f3bb50bbd88
vishkharat <154818939+vishkharat@users.noreply.github.com>
2024-04-24 10:21:26 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery',false);
+mongoose.connect('mongodb+srv://vishdcosta:8jz83Qd0UrtxAJMP@cluster0.9x6x1zp.mongodb.net/pollingsystem');
+
+// Code for using the cloud mongodb atlas 
+const connectParams={
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}
+
+// Code for manually using the mongodb of local system
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'error connecting to database'));
+
+db.once('open', ()=>{
+    console.log("successfully connected to database : mongoDB");
+});
+
+module.exports = mongoose;
---
+++ index.js
@@ -0,0 +1,18 @@
+const Port=3000
+const express=require('express')
+const bodyParser=require('body-parser')
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}))
+const db=require('./config/mongoose');
+
+// Routes
+app.use('/',require('./routes/index'));
+app.use('/',(req,res)=>{
+    res.send('welcome to polling system')
+})
+app.listen(Port,function(err){
+    if(err){
+        console.log(err);
+    }
+    console.log("server is runing ...",Port);
+})
